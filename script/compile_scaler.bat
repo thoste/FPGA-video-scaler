3509a3d4c73f6203f9462f31541f5fa11b7af673
Thomas Stenseth <stenseth.thomas@gmail.com>
2019-01-29 16:20:21 +0100

    Added build scripts

---
+++ script/compile_scaler.bat
@@ -0,0 +1,5 @@
+vlib work
+vcom -2008 -work work C:/Users/stens/Documents/NTNU/FPGA-video-scaler/src/controller.vhd
+vcom -2008 -work work C:/Users/stens/Documents/NTNU/FPGA-video-scaler/src/control_packet_decoder.vhd
+vcom -2008 -work work C:/Users/stens/Documents/NTNU/FPGA-video-scaler/src/scaler.vhd
+vcom -2008 -work work C:/Users/stens/Documents/NTNU/FPGA-video-scaler/project/../tb/scaler_tb.vhd
